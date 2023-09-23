a21df5308a1f07e0f3233d0db8a190d1a1be65b8
farooqahmad-coder <farooqui1600@gmail.com>
2023-09-23 22:20:40 +0500

    some changes

---
+++ index.js
@@ -0,0 +1,37 @@
+import inquirer from 'inquirer';
+async function calculater() {
+    var cal = await inquirer.prompt([{
+            type: "number",
+            name: "num1",
+            message: "enter your num1"
+        },
+        {
+            type: "number",
+            name: "num2",
+            message: "enter your num2"
+        },
+        {
+            type: "list",
+            name: "operation",
+            message: "enter your operator",
+            choices: ["+", "-", "*", "/"]
+        }]);
+    switch (cal.operation) {
+        case "+":
+            console.log(cal.num1 + cal.num2);
+            break;
+        case "-":
+            console.log(cal.num1 - cal.num2);
+            break;
+        case "*":
+            console.log(cal.num1 * cal.num2);
+            break;
+        case "/":
+            console.log(cal.num1 / cal.num2);
+            break;
+        default:
+            console.log("something went wrong");
+            break;
+    }
+}
+calculater();
